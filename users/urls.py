ca0f3425c877bf00c1fb11b806350eb5913a7a63
Alfarabi09 <fara2001@mail.ru>
2023-12-14 02:45:06 +0600

    done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import get_people, home_view, get_planets, get_starships
+
+app_name = 'users'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home_view, name='home'),
+    path('people', get_people, name='people'),
+    path('planets', get_planets, name='planets'),
+    path('starships/', get_starships, name='starships'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,78 @@
 from django.shortcuts import render
 import requests
+from django.core.cache import cache
 
-def index(request):
+def home_view(request):
+    return render(request, 'users/home.html')
+
+def get_people(request):
     context = {}
     if 'fetch_data' in request.POST:
-        response = requests.get('https://swapi.dev/api/people/')
-        context['characters'] = response.json()['results']
-    return render(request, 'users/index.html', context)
+        characters = cache.get('characters')
+
+        if not characters:
+            characters = []
+            url = 'https://swapi.dev/api/people/'
+            while url:
+                try:
+                    response = requests.get(url, timeout=5)
+                    response.raise_for_status()  
+                    data = response.json()
+                    characters.extend(data['results'])
+                    url = data['next']
+                except requests.RequestException as e:
+                    print(f"Ошибка запроса: {e}")  
+                    break
+
+            cache.set('characters', characters, timeout=3600)  
+
+        context['characters'] = characters
+    return render(request, 'users/people.html', context)
+
+def get_planets(request):
+    context = {}
+    if 'fetch_data' in request.POST:
+        planets = cache.get('planets')
+
+        if not planets:
+            planets = []
+            url = 'https://swapi.dev/api/planets/'
+            while url:
+                try:
+                    response = requests.get(url, timeout=5)
+                    response.raise_for_status()
+                    data = response.json()
+                    planets.extend(data['results'])
+                    url = data['next']
+                except requests.RequestException as e:
+                    print(f"Ошибка запроса: {e}")
+                    break
+
+            cache.set('planets', planets, timeout=3600)
+
+        context['planets'] = planets
+    return render(request, 'users/planets.html', context)
+
+def get_starships(request):
+    context = {}
+    if 'fetch_data' in request.POST:
+        starships = cache.get('starships')
+
+        if not starships:
+            starships = []
+            url = 'https://swapi.dev/api/starships/'
+            while url:
+                try:
+                    response = requests.get(url, timeout=5)
+                    response.raise_for_status()
+                    data = response.json()
+                    starships.extend(data['results'])
+                    url = data['next']
+                except requests.RequestException as e:
+                    print(f"Ошибка запроса: {e}")
+                    break
+                
+            cache.set('starships', starships, timeout=3600)
+
+        context['starships'] = starships
+    return render(request, 'users/starships.html', context)
